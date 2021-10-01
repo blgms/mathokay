72143de616a289e57a95b244d4244b68366d0dbb
blgms <blgmathssciences@gmail.com>
2021-10-01 19:21:57 +0200

    Essai export

--- docs/assets/js/export.js
+++ docs/assets/js/export.js
@@ -1,31 +1,32 @@
 function download(filename,text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-  element.style.display = 'none';
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
 }
 
-async getCss(css) {
+async function exportHtml() {
+	let mains = document.getElementsByTagName("main");
+	let html = "<!doctype html><html lang='fr'><head><style>";
 	let css = "";
-	await fetch("docs/assets/css/custom.css")
+	await fetch("https://blgms.github.io/mathokay/docs/assets/css/pico.min.css")
 	.then(function(response) {
 		return response.text();
 	})
 	.then(function(data) {
-		css = data;
+		css += data;
 	});
-	return css;
-}
-
-function exportHtml() {
-	let headers = document.getElementsByTagName("header");
-	let mains = document.getElementsByTagName("main");
-	let html = "<!doctype html><html lang='fr'><head><style>";
-	let css = getCss();
+	await fetch("https://blgms.github.io/mathokay/docs/assets/css/custom.css")
+	.then(function(response) {
+		return response.text();
+	})
+	.then(function(data) {
+		css += data;
+	});
 	console.log(css);
-	html += css+"</style></head><body><header>"+headers[0].innerHTML+"</header><main class='container pad6'>"+mains[0].innerHTML+"</main></body></html>";
+	html += css+"</style></head><body><header>POUET</header><main class='container pad6'>"+mains[0].innerHTML+"</main></body></html>";
 	download("hello.html",html);
 }
