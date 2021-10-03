aeffcfcd99130018ddbfa9bb3960476ca7078921
blgms <blgmathssciences@gmail.com>
2021-10-03 12:31:33 +0200

    Ajout d'un lien vers la librairie MathJax dans les fichiers exportés pour le rendu sous Chrome

--- docs/assets/js/export.js
+++ docs/assets/js/export.js
@@ -10,6 +10,7 @@ function download(filename,text) {
 
 async function exportHtml() {
 	let jsExp = "<script type='text/javascript'>var slon = 2;function slider(sl) {if (sl!=slon) {document.getElementById('slide'+slon).hidden=true;document.getElementById('btn'+slon).hidden=true;slon = sl;document.getElementById('slide'+slon).hidden=false;document.getElementById('btn'+slon).hidden=false;}}</script>";
+	jsExp += "<script src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script><script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>";
 	let date = [new Date()];
 	date.push(date[0].getDate(), date[0].getMonth()+1, date[0].getFullYear());
 	let tempDiv = document.createElement('div');
@@ -36,7 +37,7 @@ async function exportHtml() {
 	.then(function(data) {
 		css += data;
 	});
-	let html = "<!doctype html><html lang='fr'><head>"+jsExp+"<style>"+css+".katex-mathml { font-size:1.3rem; }</style></head><body><header><nav class='container-fluid'><ul><li id='liLogo'><div id='divLogo'><hgroup><h4>Math'Okay</h4><h5>Séance du "+date[1]+"/"+date[2]+"/"+date[3]+"</h5></hgroup></div></li></ul><ul><li id='btn3' hidden><button style='width:7rem;' onclick='slider(2)'>Questions</button></li><li id='btn2'><button style='width:7rem;' onclick='slider(3)'>Réponses</button></li></ul></nav></header><main class='container pad6'>"+tempDiv.innerHTML+"</main></body></html>";
+	let html = "<!doctype html><html lang='fr'><head>"+jsExp+"<style>"+css+"</style></head><body><header><nav class='container-fluid'><ul><li id='liLogo'><div id='divLogo'><hgroup><h4>Math'Okay</h4><h5>Séance du "+date[1]+"/"+date[2]+"/"+date[3]+"</h5></hgroup></div></li></ul><ul><li id='btn3' hidden><button style='width:7rem;' onclick='slider(2)'>Questions</button></li><li id='btn2'><button style='width:7rem;' onclick='slider(3)'>Réponses</button></li></ul></nav></header><main class='container pad6'>"+tempDiv.innerHTML+"</main></body></html>";
 	download("Automatismes-"+date[1]+"-"+date[2]+"-"+date[3]+".html",html);
 	document.body.removeChild(tempDiv);
 }
