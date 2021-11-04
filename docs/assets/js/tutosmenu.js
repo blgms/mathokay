c482a8e6e5b32dbc7a34dddfa57ec77d779e962f
blgms <blgmathssciences@gmail.com>
2021-11-04 17:15:48 +0100

    Modification non publique

--- docs/assets/js/tutosmenu.js
+++ docs/assets/js/tutosmenu.js
@@ -1,4 +1,36 @@
 var slon = 1;
+var page, nbPages;
+
+function chargePage() {
+	let query = window.location.search;
+	let params = new URLSearchParams(query);
+	let refTuto = params.get('page');
+	if (refTuto) {
+		if (UrlExists("https://blgms.github.io/mathokay/docs/assets/tutos/"+refTuto+".html") == true) {
+			afficheTuto(refTuto);
+		}
+	}
+}
+
+function UrlExists(url) {
+	var http = new XMLHttpRequest();
+	http.open('HEAD', url, false);
+	http.send();
+	return http.status!=404;
+}
+
+async function afficheTuto(ref) {
+	await fetch("https://blgms.github.io/mathokay/docs/assets/tutos/"+ref+".html")
+	.then(function(response) {
+		return response.text();
+	})
+	.then(function(data) {
+		document.getElementById("articleTuto").innerHTML = data;
+		nbPages = document.getElementById("corpsTuto").childElementCount;
+		page = 1;
+		slider(3);
+	});
+}
 
 function slider(sl) {
 	if (sl!=slon) {
@@ -22,13 +54,10 @@ function mainMenu() {
 	centerScreen.hidden=!centerScreen.hidden;
 }
 
-async function afficheTuto() {
-	await fetch("https://blgms.github.io/mathokay/docs/assets/tutos/ti_stat1.html")
-	.then(function(response) {
-		return response.text();
-	})
-	.then(function(data) {
-		document.getElementById("tutoAffiche").innerHTML = data;
-		slider(3);
-	});
+function tutoPage(n) {
+	if ((page >= 1 && page < nbPages && n == 1) || (page > 1 && page <= nbPages && n == -1)) {
+		document.getElementById("tutoPage"+page).hidden = true;
+		page += n;
+		document.getElementById("tutoPage"+page).hidden = false;
+	}
 }
